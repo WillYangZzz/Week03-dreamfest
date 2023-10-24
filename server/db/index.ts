16e996e496bd72fa2628abb8ca5465f4492467ee
Kirsty <142766386+Kirsty-Ammundsen@users.noreply.github.com>
2023-10-25 11:29:53 +1300

    Add all events

--- server/db/index.ts
+++ server/db/index.ts
@@ -15,4 +15,28 @@ export async function getAllLocations() {
   return await db('locations').select()
 }
 
+export async function getEventDetails() {
+  return await db('events')
+    .join('locations', 'locations.id', 'events.location_id')
+    .select(
+      'events.id as id',
+      'day',
+      'time',
+      'events.name as eventName',
+      'events.description as description',
+      'locations.name as locationName'
+    )
+}
+
+// events: [
+//     {
+//       id: 1,
+//       day: 'friday',
+//       time: '2pm - 3pm',
+//       eventName: 'Slushie Apocalypse I',
+//       description:
+//         'This is totally a description of this really awesome event that will be taking place during this festival at the TangleStage. Be sure to not miss the free slushies cause they are rad!',
+//       locationName: 'TangleStage',
+//     },
+
 // TODO: write some more database functions
