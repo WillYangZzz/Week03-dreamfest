693faa143bb99336eab37875e9aa1ef4009855cf
Will <105895841+WillYangZzz@users.noreply.github.com>
2023-10-25 12:10:55 +1300

    show events for a day

--- server/db/index.ts
+++ server/db/index.ts
@@ -8,10 +8,38 @@ type Environment = 'production' | 'test' | 'development'
 const environment = (process.env.NODE_ENV || 'development') as Environment
 const config = knexFile[environment]
 
-export const connection = knex(config)
+export const db = knex(config)
+console.log(db)
 
 export async function getAllLocations() {
   // TODO: use knex to get the real location data from the database
+
+  return (await db('locations').select(
+    'id',
+    'name as locationName',
+    'description'
+  )) as LocationData[]
 }
 
 // TODO: write some more database functions
+
+export async function getLocationById(id: number) {
+  return (await db('locations').where('id', id).select()) as LocationData[]
+}
+
+export async function getEventsByDay(day: string) {
+  const events = await db('events')
+    .join('locations', 'locations.id', 'events.location_id')
+    .where('day', day)
+    .select(
+      'events.id',
+      'day',
+      'time',
+      'events.name as eventName',
+      'events.description',
+      'locations.name as locationName'
+    )
+  console.log(events)
+}
+
+getEventsByDay('friday')
--- server/routes/locations.ts
+++ server/routes/locations.ts
@@ -5,38 +5,22 @@ import * as db from '../db/index.ts'
 const router = express.Router()
 
 // GET /locations
-router.get('/', (req, res) => {
-  // TODO: Replace this with all of the locations in the database
-  const locations = [
-    {
-      id: 1,
-      name: 'TangleStage',
-      description:
-        'Not the biggest stage, but perhaps the most hip. Not the biggest stage, but perhaps the most hip. Not the biggest stage, but perhaps the most hip.',
-    },
-    {
-      id: 2,
-      name: 'Yella Yurt',
-      description:
-        "It's a freakin' yurt! Get in here! It's a freakin' yurt! Get in here! It's a freakin' yurt! Get in here! It's a freakin' yurt! Get in here!",
-    },
-  ]
+router.get('/', async (req, res) => {
+  const locations = await db.getAllLocations()
 
   const viewData = { locations }
   res.render('showLocations', viewData)
 })
 
 // GET /locations/4/edit
-router.get('/:id/edit', (req, res) => {
+router.get('/:id/edit', async (req, res) => {
   const id = Number(req.params.id)
 
   // TODO: Get the location based on its id and replace this viewData
-  const viewData = {
-    id: id,
-    name: 'TangleStage',
-    description:
-      'Not the biggest stage, but perhaps the most hip. Not the biggest stage, but perhaps the most hip. Not the biggest stage, but perhaps the most hip.',
-  }
+  const location = await db.getLocationById(id)
+
+  const viewData = { location }
+  console.log(viewData)
 
   res.render('editLocation', viewData)
 })
