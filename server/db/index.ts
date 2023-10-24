5abe927529a70762155edcc2eb027bd336a858fd
Logan Bruce <l.t.bruce6739@gmail.com>
2023-10-25 11:56:08 +1300

    completed the getAllLocations fucntion

--- server/db/index.ts
+++ server/db/index.ts
@@ -2,6 +2,7 @@ import knexFile from './knexfile.js'
 import knex from 'knex'
 import type { Location, LocationData } from '../../models/Location.ts'
 import type { Event, EventData, EventWithLocation } from '../../models/Event.ts'
+// import * as db from '../index.ts'
 
 type Environment = 'production' | 'test' | 'development'
 
@@ -12,6 +13,9 @@ export const connection = knex(config)
 
 export async function getAllLocations() {
   // TODO: use knex to get the real location data from the database
+  return await connection('locations').select('*')
 }
+
+getAllLocations()
 
 // TODO: write some more database functions
