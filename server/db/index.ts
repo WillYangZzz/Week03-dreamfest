e7d6762d15ab252fa4b01e8f8f57afb6f0067b4c
Ricky <65267289+ricky-huang-dev@users.noreply.github.com>
2023-10-25 14:17:54 +1300

    add get all location and events by day functionality

--- server/db/index.ts
+++ server/db/index.ts
@@ -2,16 +2,29 @@ import knexFile from './knexfile.js'
 import knex from 'knex'
 import type { Location, LocationData } from '../../models/Location.ts'
 import type { Event, EventData, EventWithLocation } from '../../models/Event.ts'
-
 type Environment = 'production' | 'test' | 'development'
 
 const environment = (process.env.NODE_ENV || 'development') as Environment
 const config = knexFile[environment]
 
-export const connection = knex(config)
+export const db = knex(config)
 
 export async function getAllLocations() {
-  // TODO: use knex to get the real location data from the database
+  // Done: use knex to get the real location data from the database
+  return (await db('locations').select()) as LocationData[]
 }
 
 // TODO: write some more database functions
+export async function getEventsByDay(day: string) {
+  return (await db('events')
+    .join('locations', 'events.location_id', 'locations.id')
+    .select(
+      'events.id as id',
+      'events.day as day',
+      'events.time as time',
+      'events.name as eventName',
+      'locations.name as locationName',
+      'events.description as description'
+    )
+    .where('events.day', day)) as EventData[]
+}
