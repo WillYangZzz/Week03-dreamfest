3ed7245cbac4cc3cda8eea4adbe96ee600f91992
Xavier <143090232+xavier-kirkpatrick@users.noreply.github.com>
2023-10-25 15:15:45 +1300

    add show all locations and events per day

--- server/db/index.ts
+++ server/db/index.ts
@@ -8,10 +8,26 @@ type Environment = 'production' | 'test' | 'development'
 const environment = (process.env.NODE_ENV || 'development') as Environment
 const config = knexFile[environment]
 
-export const connection = knex(config)
+export const db = knex(config)
 
 export async function getAllLocations() {
   // TODO: use knex to get the real location data from the database
+  return (await db('locations').select()) as LocationData[]
+  //console.log(dbLocations)
 }
 
 // TODO: write some more database functions
+
+export async function getEventsByDay(day: string) {
+  return await db('locations')
+    .join('events', 'locations.id', 'events.location_id')
+    .where('day', day)
+    .select(
+      'events.id',
+      'events.name as eventName',
+      'events.day',
+      'events.description',
+      'locations.name as locationName',
+      'events.time'
+    )
+}
