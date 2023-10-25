513077eed663d473b8d8509b3d62c715d03f9749
Pathik Modi <modipathik95@gmail.com>
2023-10-25 14:22:58 +1300

    update location details

--- server/db/index.ts
+++ server/db/index.ts
@@ -2,16 +2,56 @@ import knexFile from './knexfile.js'
 import knex from 'knex'
 import type { Location, LocationData } from '../../models/Location.ts'
 import type { Event, EventData, EventWithLocation } from '../../models/Event.ts'
+import { logDOM } from '@testing-library/dom'
 
 type Environment = 'production' | 'test' | 'development'
 
 const environment = (process.env.NODE_ENV || 'development') as Environment
 const config = knexFile[environment]
 
-export const connection = knex(config)
+export const db = knex(config)
+
+// interface Location {
+//   id: number
+//   name: string
+//   description: string
+// }
 
 export async function getAllLocations() {
   // TODO: use knex to get the real location data from the database
+  return (await db('locations').select(
+    'id',
+    'name',
+    'description'
+  )) as Location[]
 }
 
 // TODO: write some more database functions
+// TODO: use knex to get the real location data from the database
+
+export async function getDayEvents(day: string) {
+  return (await db('events')
+    .where('day', day)
+    .select(
+      'id',
+      'location_id',
+      'day',
+      'time',
+      'name',
+      'description'
+    )) as EventData[]
+}
+
+export async function getLocationById(id: number) {
+  return (await db('locations')
+    .where('id', id)
+    .select('id', 'name', 'description')
+    .first()) as Location[]
+}
+
+export async function updateLocation(updatedLocation: Location) {
+  await db('locations').where('id', updatedLocation.id).update({
+    name: updatedLocation.name,
+    description: updatedLocation.description,
+  })
+}
--- server/routes/locations.ts
+++ server/routes/locations.ts
@@ -5,49 +5,53 @@ import * as db from '../db/index.ts'
 const router = express.Router()
 
 // GET /locations
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // TODO: Replace this with all of the locations in the database
-  const locations = [
-    {
-      id: 1,
-      name: 'TangleStage',
-      description:
-        'Not the biggest stage, but perhaps the most hip. Not the biggest stage, but perhaps the most hip. Not the biggest stage, but perhaps the most hip.',
-    },
-    {
-      id: 2,
-      name: 'Yella Yurt',
-      description:
-        "It's a freakin' yurt! Get in here! It's a freakin' yurt! Get in here! It's a freakin' yurt! Get in here! It's a freakin' yurt! Get in here!",
-    },
-  ]
+  // const locations = [
+  //   {
+  //     id: 1,
+  //     name: 'TangleStage',
+  //     description:
+  //       'Not the biggest stage, but perhaps the most hip. Not the biggest stage, but perhaps the most hip. Not the biggest stage, but perhaps the most hip.',
+  //   },
+  //   {
+  //     id: 2,
+  //     name: 'Yella Yurt',
+  //     description:
+  //       "It's a freakin' yurt! Get in here! It's a freakin' yurt! Get in here! It's a freakin' yurt! Get in here! It's a freakin' yurt! Get in here!",
+  //   },
+  // ]
+  // const viewData = { locations }
+  // res.render('showLocations', viewData)
 
-  const viewData = { locations }
-  res.render('showLocations', viewData)
+  const locationArray = await db.getAllLocations()
+  const viewLocation = { locations: locationArray }
+  // console.log(viewLocation)
+  res.render('showLocations', viewLocation)
 })
 
 // GET /locations/4/edit
-router.get('/:id/edit', (req, res) => {
+router.get('/:id/edit', async (req, res) => {
   const id = Number(req.params.id)
 
   // TODO: Get the location based on its id and replace this viewData
-  const viewData = {
-    id: id,
-    name: 'TangleStage',
-    description:
-      'Not the biggest stage, but perhaps the most hip. Not the biggest stage, but perhaps the most hip. Not the biggest stage, but perhaps the most hip.',
-  }
-
+  // const viewData = {
+  //   id: id,
+  //   name: 'TangleStage',
+  //   description:
+  //     'Not the biggest stage, but perhaps the most hip. Not the biggest stage, but perhaps the most hip. Not the biggest stage, but perhaps the most hip.',
+  // }
+  const viewData = await db.getLocationById(id)
   res.render('editLocation', viewData)
 })
 
 // POST /locations/edit
-router.post('/edit', (req, res) => {
+router.post('/edit', async (req, res) => {
   // ASSISTANCE: So you know what's being posted ;)
-  // const { id, name, description } = req.body
+  const updatedLocation = req.body
+  await db.updateLocation(updatedLocation)
 
   // TODO: Update the location in the database based on its id
-
   res.redirect('/locations')
 })
 
