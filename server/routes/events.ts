cb46b28fcba71d9f0e0807d7186dd6cfc8d0033e
Pathik Modi <modipathik95@gmail.com>
2023-11-20 11:27:00 +1300

    removed comments and unwanted examples and streamline functions

--- server/routes/events.ts
+++ server/routes/events.ts
@@ -14,19 +14,6 @@ router.get('/add/:day', async (req, res) => {
     name: capitalise(eventDay),
     selected: eventDay === day ? 'selected' : '',
   }))
-
-  // TODO: Replace this with all of the locations in the database
-  // const locations = [
-  //   {
-  //     id: 1,
-  //     name: 'TangleStage',
-  //   },
-  //   {
-  //     id: 2,
-  //     name: 'Yella Yurt',
-  //   },
-  // ]
-
   const locations = await db.getAllLocations() // this line replaces the above location array of objects
   const viewData = { locations, days, day }
   res.render('addEvent', viewData)
@@ -38,11 +25,6 @@ router.post('/add', async (req, res) => {
   const event = req.body
   const day = validateDay(req.body.day)
   await db.addNewEvent(event)
-
-  // TODO: Add the event to the database and then redirect
-
-  // const day = 'friday' // TODO: Remove this line
-
   res.redirect(`/schedule/${day}`)
 })
 
@@ -52,18 +34,13 @@ router.post('/delete', async (req, res) => {
   const day = validateDay(req.body.day)
   await db.deleteEvent(id)
   // TODO: Delete the event from the database using its id
-
-  // const day = 'friday' // TODO: Remove this line
-
   res.redirect(`/schedule/${day}`)
 })
 
 // GET /events/3/edit
 router.get('/:id/edit', (req, res) => {
   const id = Number(req.params.id)
-
   // TODO: Replace event below with the event from the database using its id
-  // NOTE: It should have the same shape as this one
   const event = {
     id: id,
     locationId: 1,
@@ -75,9 +52,6 @@ router.get('/:id/edit', (req, res) => {
   }
 
   // TODO: Replace locations below with all of the locations from the database
-  // NOTE: The objects should have the same shape as these.
-  // The selected property should have a value of
-  // either 'selected' or '' based on event.locationId above.
   const locations = [
     { id: 1, name: 'TangleStage', selected: '' },
     { id: 2, name: 'Yella Yurt', selected: 'selected' },
@@ -99,11 +73,6 @@ router.get('/:id/edit', (req, res) => {
 // POST /events/edit
 router.post('/edit', (req, res) => {
   // ASSISTANCE: So you know what's being posted ;)
-  // const { name, description, time } = req.body
-  // const id = Number(req.body.id)
-  // const day = validateDay(req.body.day)
-  // const locationId = Number(req.body.locationId)
-
   // TODO: Update the event in the database using the identifiers created above
 
   const day = 'friday' // TODO: Remove this line
