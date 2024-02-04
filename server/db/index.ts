aa74d5de531d7421e7560fb75dbdb1c8f7d976eb
Will Yang <willyangzzz@gmail.com>
2024-02-04 17:39:34 +1300

    create getAllLocations function

--- server/db/index.ts
+++ server/db/index.ts
@@ -8,10 +8,16 @@ type Environment = 'production' | 'test' | 'development'
 const environment = (process.env.NODE_ENV || 'development') as Environment
 const config = knexFile[environment]
 
-export const connection = knex(config)
+export const db = knex(config)
 
+// Locations
 export async function getAllLocations() {
   // TODO: use knex to get the real location data from the database
+  return (await db('locations').select(
+    'id',
+    'name',
+    'description'
+  )) as Location[]
 }
 
 // TODO: write some more database functions
